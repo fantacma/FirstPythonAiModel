09c0187f0566ddb6a1176bbb27bec0578ae0dcd1
fantacma <o.davkaa@gmail.com.com>
2025-03-12 16:44:55 +0800

    Apply .gitignore properly

---
+++ FirstPythonAiModel/FirstPythonAiModel/FirstPythonAiModel.py
@@ -0,0 +1,8 @@
+﻿import os
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "mistralai/Mistral-7B-v0.1"
+token = os.getenv("HF_TOKEN")  # Token-ийг орчны хувьсагчид хадгална уу
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, token=token)
+model = AutoModelForCausalLM.from_pretrained(model_name, token=token)
